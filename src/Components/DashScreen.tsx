ee276386715694a232cbad2921cea1c7fd5e67af
mbruty <michael.bruty@students.plymouth.ac.uk>
2020-08-05 16:07:00 +0100

    :sparkles: Display lists

--- src/Components/DashScreen.tsx
+++ src/Components/DashScreen.tsx
@@ -1,30 +1,46 @@
 import React, { Component } from 'react'
-import { Board } from '../Types/Board';
+import { List, Card } from '../Types/List';
 import AddNewList from './AddNewList';
-
+import ListCard from './ListCard';
+import { reorder } from '../Helper-Functions/ReorderLists'
+import testLists from '../list.json'
+import { json } from 'express';
 export default class DashScreen extends Component<DashProps, DashState> {
 
     constructor(props: DashProps){
         super(props);
+        let data: Array<List> = JSON.parse(JSON.stringify(testLists));
         this.state = {
-            boards: [],
+            lists: data,
             projectName: ''
         }
 
         this.updateBoards = this.updateBoards.bind(this);
+        this.updateList = this.updateList.bind(this); 
     }
 
     updateBoards(boardName: string){
-        let boardsCoppy: Array<Board> = this.state.boards;
-        boardsCoppy.push({name: boardName, listPosition: boardsCoppy.length});
-        this.setState({...this.state, boards: boardsCoppy})
+        let listCopy: Array<List> = this.state.lists;
+        listCopy.push({id: 0, name: boardName, listPosition: listCopy.length, items: []});
+        this.setState({...this.state, lists: reorder(listCopy)});
+    }
+
+    updateList(id: number, name: string){
+        //Find the list in current state
+        const list = this.state.lists.find(l => l.id === id);
+        const newItem : Card = {
+            name: name,
+            complete: false,
+            itemPosition: list!.items.length
+        }
+        list?.items.push(newItem);
     }
     render() {
         return (
             <div className="dash-screen">
                 <div className="card-container">
-                    {this.state.boards.map(board => (
-                        null
+                    {this.state.lists.map(list => (
+                        <ListCard updateList={this.updateList} list={list}/>
                     ))}
                     <AddNewList callback={this.updateBoards}/>
                 </div>
@@ -38,6 +54,6 @@ interface DashProps{
 }
 
 interface DashState{
-    boards: Array<Board>,
+    lists: Array<List>,
     projectName: string
 }
