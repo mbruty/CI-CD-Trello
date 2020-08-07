444f374e2c749e01663cf40ce3c3b8017186bb66
mbruty <michael.bruty@students.plymouth.ac.uk>
2020-08-07 21:39:06 +0100

    Changes to reflect new structure

--- src/api/schemas/ListSchema.js
+++ src/api/schemas/ListSchema.js
@@ -7,7 +7,7 @@ const schema = yup.object().shape({
         listPosition: yup.number().required(),
         items: yup.array().of(yup.object().shape({
                 name: yup.string().required(),
-                label: yup.string().matches(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/),
+                label: yup.array().of(yup.string().matches(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/)),
                 due: yup.date(),
                 complete: yup.boolean().required(),
                 itemPosition: yup.number().integer().required()
