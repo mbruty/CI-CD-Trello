e5021a2879e17c69f0adb997639078099b9b752b
mbruty <michael.bruty@students.plymouth.ac.uk>
2020-08-07 21:39:33 +0100

    :sparkles: Showing modal

--- src/Components/DashScreen.tsx
+++ src/Components/DashScreen.tsx
@@ -5,26 +5,33 @@ import List from './List';
 import { reorder } from '../Helper-Functions/ReorderLists'
 import axios from 'axios';
 import { updateBoard } from '../Fetch-Functions/UpdateBoards'
+import OptionsModal from '../Modals/OptionsModal'
+
 export default class DashScreen extends Component<DashProps, DashState> {
 
     constructor(props: DashProps){
         super(props);
         this.state = {
             lists: [],
+            colours: [],
             projectName: '',
             fetched: false,
-            boardID: this.props.boardID
+            boardID: this.props.boardID,
+            showOptionsModal: false,
+            cardToShow: undefined,
         }
-
         this.updateBoards = this.updateBoards.bind(this);
-        this.updateList = this.updateList.bind(this); 
+        this.addItemToList = this.addItemToList.bind(this); 
         this.fetchData = this.fetchData.bind(this);
+        this.updateCardInList = this.updateCardInList.bind(this);
+        this.showOptionsModal = this.showOptionsModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     async fetchData(){
         axios.get('http://localhost:5000/board/' + this.props.boardID)
         .then(response => {
-            this.setState({...this.state, lists: response.data.lists, fetched: true});
+            this.setState({...this.state, lists: response.data.lists, colours: response.data.lables, fetched: true});
         })
     }
 
@@ -36,23 +43,50 @@ export default class DashScreen extends Component<DashProps, DashState> {
         this.setState({...this.state, lists: reorder(listCopy)});
     }
 
-    updateList(id: number, name: string){
+    addItemToList(id: number, name: string){
         let listCopy: Array<ListType> = [...this.state.lists]
-        listCopy.forEach(element => {
-            if(element.id === id){
-                const newItem : CardType = {
-                    name: name,
-                    complete: false,
-                    itemPosition: element.items.length
+        axios.get('http://localhost:5000/board/' + this.props.boardID + '/max').then( max => {
+            listCopy.forEach(element => {
+                if(element.id === id){
+                    const newItem : CardType = {
+                        name: name,
+                        id: max.data + 1,
+                        complete: false,
+                        itemPosition: element.items.length
+                    }
+                    element.items.push(newItem);
                 }
-                element.items.push(newItem);
-            }
+            });
+            this.setState({...this.state, lists: listCopy});
+            // // Update the database with the new item in the list
+            updateBoard(this.props.boardID, listCopy).then( r => console.log("r"))
+            .catch(err => console.log(err))
+
         });
-        this.setState({...this.state, lists: listCopy});
-        // // Update the database with the new item in the list
-        updateBoard(this.props.boardID, listCopy).then( r => console.log("r"))
-        .catch(err => console.log(err))
     }
+
+    showOptionsModal(card: CardType) {
+        this.setState({...this.state, showOptionsModal: true, cardToShow: card});
+    }
+
+    closeModal() {
+        this.setState({...this.state, showOptionsModal: false, cardToShow: undefined});
+    }
+
+    updateCardInList(id: number, newName: string){
+        let listCopy: Array<ListType> = [...this.state.lists]
+        listCopy.forEach(list => {
+            list.items.forEach(item =>{
+                if(item.id === id){
+                    item.name = newName;
+                }
+            })
+        })
+        this.setState({...this.state, lists: listCopy});
+        updateBoard(this.props.boardID, listCopy).then( r => console.log("r"))
+            .catch(err => console.log(err))
+    }
+
     render() {
         if(this.props.boardID && !this.state.fetched){
             this.fetchData();
@@ -61,10 +95,11 @@ export default class DashScreen extends Component<DashProps, DashState> {
             <div className="dash-screen">
                 <div className="card-container">
                     {this.state.lists.map(list => (
-                        <List updateList={this.updateList} list={list}/>
+                        <List colours={this.state.colours} showOptionsModal={this.showOptionsModal} updateCardInList={this.updateCardInList} updateList={this.addItemToList} list={list}/>
                     ))}
                     <AddNewList callback={this.updateBoards}/>
                 </div>
+                {this.state.showOptionsModal ? <OptionsModal colours={this.state.colours} close={this.closeModal} card={this.state.cardToShow}/> : null}
             </div>
         )
     }
@@ -76,7 +111,10 @@ interface DashProps{
 
 interface DashState{
     lists: Array<ListType>,
+    colours: Array<string>,
     projectName: string,
     fetched: boolean,
-    boardID: string
+    boardID: string,
+    showOptionsModal: boolean,
+    cardToShow?: CardType
 }
